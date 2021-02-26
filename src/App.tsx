21d67b06424f58667500c848c39e75b76c889899
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2021-02-26 22:52:29 +0000

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BasicLayout from './components/BasicLayout';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <BasicLayout>
+        <Switch>
+          <Route path="/1">
+            <h1>1</h1>
+          </Route>
+          <Route path="/2">
+            <h1>2</h1>
+          </Route>
+          <Route path="/3">
+            <h1>3</h1>
+          </Route>
+        </Switch>
+      </BasicLayout>
+    </Router>
   );
-}
+};
 
 export default App;
