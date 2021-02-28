877ec056de35fc9f7fa200608600122f66c69923
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2021-02-28 22:55:06 +0000

    Add menu item link with pathname

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MenuStyled, MenuItemStyled } from './styles/Drawer';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Drawer: React.FC = () => {
-  const topics: string[] = ['1', '2', '3'];
+  const location = useLocation();
 
-  const styledTopics: JSX.Element[] = [];
+  const items = [
+    { key: '1', label: 'Dashboard', path: '/admin/dashboard' },
+    { key: '2', label: 'Meus Lotes', path: '/admin/lotes' },
+    { key: '3', label: 'Entregar/Receber lote', path: '/admin/ler-qrcode' },
+    { key: '4', label: 'Pesquisar QR Code', path: '/admin/pesquisar-qrcode' },
+  ];
 
-  topics.forEach((topic: string, index: number) =>
-    styledTopics.push(
-      <MenuItemStyled key={index}>
-        <Link to={topic}>{topic}</Link>
-      </MenuItemStyled>,
-    ),
+  const [selectedKey, setSelectedKey] = useState(
+    items.find((_item) => location.pathname.startsWith(_item.path))?.key,
   );
 
+  useEffect(() => {
+    setSelectedKey(
+      items.find((_item) => location.pathname.startsWith(_item.path))?.key,
+    );
+  }, [location]);
+
   return (
-    <MenuStyled theme="dark" mode="inline" defaultSelectedKeys={['0']}>
-      {styledTopics}
+    <MenuStyled
+      theme="dark"
+      mode="inline"
+      selectedKeys={selectedKey ? [selectedKey] : ['1']}
+    >
+      {items.map((item) => (
+        <MenuItemStyled key={item.key}>
+          <Link to={item.path}>{item.label}</Link>
+        </MenuItemStyled>
+      ))}
     </MenuStyled>
   );
 };
