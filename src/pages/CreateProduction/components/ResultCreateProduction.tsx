c984ab45103c0157110ce09bff3036579ccd826e
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2021-03-11 22:47:50 +0000

    Add btn "ver minhas produções"

--- src/pages/CreateProduction/components/ResultCreateProduction.tsx
+++ src/pages/CreateProduction/components/ResultCreateProduction.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Button, Result } from 'antd';
 import { useSelector } from 'react-redux';
 import { CreateProductionContext } from '../CreateProduction';
+import { Link } from 'react-router-dom';
 
 const ResultCreateProduction: React.FC = () => {
   const { error } = useSelector((state: any) => state.production);
@@ -17,8 +18,12 @@ const ResultCreateProduction: React.FC = () => {
           title="Erro!"
           subTitle="Alguma coisa deu errado."
           extra={[
-            <Button key="novamente" onClick={() => setResult(false)}>
-              Criar outra produção
+            <Button
+              key="novamente"
+              type="primary"
+              onClick={() => setResult(false)}
+            >
+              Tentar novamente
             </Button>,
           ]}
         />
@@ -29,8 +34,11 @@ const ResultCreateProduction: React.FC = () => {
           title="Sucesso!"
           subTitle="Sua produção foi criada com sucesso."
           extra={[
-            <Button key="btn" onClick={() => setResult(false)}>
+            <Button key="btn" type="primary" onClick={() => setResult(false)}>
               Criar outra produção
+            </Button>,
+            <Button key="btn" onClick={() => setResult(false)}>
+              <Link to={'/admin/producoes'}>Ver minhas produções</Link>
             </Button>,
           ]}
         />
