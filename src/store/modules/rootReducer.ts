05749b20767532adde12d8dfe14715314c0df0c1
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2021-04-20 16:47:36 +0100

    Add auth, employee and profile

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,9 +1,15 @@
 import { combineReducers } from 'redux';
 
+import auth from './auth/reducer';
 import batch from './batch/reducer';
 import production from './production/reducer';
+import employee from './employee/reducer';
+import profile from './profile/reducer';
 
 export default combineReducers({
+  auth,
   batch,
   production,
+  employee,
+  profile,
 });
--- src/store/modules/rootSaga.ts
+++ src/store/modules/rootSaga.ts
@@ -1,7 +1,10 @@
 import { all } from 'redux-saga/effects';
+import auth from './auth/sagas';
 import batch from './batch/sagas';
 import production from './production/sagas';
+import employee from './employee/sagas';
+import profile from './profile/sagas';
 
 export default function* rootSaga(): any {
-  return yield all([batch, production]);
+  return yield all([batch, production, auth, employee, profile]);
 }
